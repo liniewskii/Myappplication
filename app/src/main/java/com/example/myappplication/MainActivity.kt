4aef9c25b1732e469b22100ebd16bd737b12ecc1
liniewskii <liniewskii@o2.pl>
2023-09-15 09:10:36 +0200

    pole textr

--- app/src/main/java/com/example/myappplication/MainActivity.kt
+++ app/src/main/java/com/example/myappplication/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.myappplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //
+        findViewById<Button>(R.id.mojprzycisk).setOnClickListener {
+            findViewById<Button>(R.id.mojprzycisk).text=
+            findViewById<TextInputEditText>(R.id.mojtekst).text
+
+            //findViewById<Button>R.id.moprzycisk).text = "Dziala"
+        }
     }
 }
